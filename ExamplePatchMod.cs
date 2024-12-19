ecdcb94919eaaefdfeb006ab8ca6776a3c6215f0
jixxed <jixxed@jixxed.nl>
2024-12-19 11:36:07 +0100

    update with config example for 1.0.25.0

--- ExamplePatchMod.cs
+++ ExamplePatchMod.cs
@@ -3,11 +3,19 @@ using StationeersMods.Interface;
 
 namespace examplepatchmod
 {
+    [StationeersMod("ExamplePatchMod","ExamplePatchMod [StationeersMods]","0.2.4657.21547.1")]
     class ExamplePatchMod : ModBehaviour
     {
+        // private ConfigEntry<bool> configBool;
         public override void OnLoaded(ContentHandler contentHandler)
         {
             //READ THE README FIRST! 
+            
+            //Config example
+            // configBool = Config.Bind("Input",
+            //     "Boolean",
+            //     true,
+            //     "Boolean description");
             
             // Harmony harmony = new Harmony("ExamplePatchMod");
             // harmony.PatchAll();
